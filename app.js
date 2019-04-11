c73518629a5f2f47c732d2d595972795dffc5ea0
Zijian Zhao <zhaozijian1219@gmail.com>
2019-04-11 14:33:52 -0700

    modified app.js

--- app.js
+++ app.js
@@ -1,67 +1,75 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
+const smallUserWord = "user".fontsize(3).sub();
+const smallCompWord = "comp".fontsize(3).sub();
 const rock_paper_scissors = {
   'r':0,
   'p':1,
   's':2
 }
 
-// console.log(rock_paper_scissors['s']);
-
 var randomProperty = function (obj) {
     var keys = Object.keys(obj);
     return Object.keys(obj)[keys.length * Math.random() << 0];
 };
 
+function convetToWord(letter){
+  if(letter === 'r') return "Rock";
+  if(letter === 'p') return "Paper";
+  return "Scissors";
+
+}
+
+
+function win(userChoice, computerChoice){
+  console.log("win!");
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_p.innerHTML = convetToWord(userChoice) + smallUserWord + " beats " + convetToWord(computerChoice) + smallCompWord + ". You win! 😜"
+}
+
+function lose(userChoice, computerChoice){
+  console.log("lose!");
+  computerScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_p.innerHTML = convetToWord(computerChoice) + smallCompWord + " beats " + convetToWord(userChoice) + smallUserWord + ". You lose! 😢"
+}
+
+function draw(userChoice, computerChoice){
+  console.log("draw!");
+  result_p.innerHTML = "Come on! It's a draw! 🤨"
+}
+
+
 function game(userChoice){
-  var computer_choice = randomProperty(rock_paper_scissors);
-  console.log(computer_choice);
-  if (userChoice === "r"){
-    switch (computer_choice) {
-      case "r":
-        console.log("Draw!!");
-        break;
-      case "p":
-        console.log("Paper covers rock. You lose!!");
-        break;
-      case "s":
-        console.log("Rock breaks scissors. You win!!");
-        break;
-    }
+  const computerChoice = randomProperty(rock_paper_scissors);
+  switch (userChoice + computerChoice) {
+    case "rs":
+    case "pr":
+    case "sp":
+      win(userChoice,computerChoice);
+      break;
+    case "rp":
+    case "ps":
+    case "sr":
+      lose(userChoice,computerChoice);
+      break;
+    case "rr":
+    case "pp":
+    case "ss":
+      draw(userChoice,computerChoice);
+      break;
+
   }
-    if (userChoice === "p"){
-      switch (computer_choice) {
-        case "p":
-          console.log("Draw!!");
-          break;
-        case "r":
-          console.log("Paper covers rock. You win!!");
-          break;
-        case "s":
-          console.log("Scissors cut paper. You lose!!");
-          break;
-        }
-      }
-      if (userChoice === "s"){
-        switch (computer_choice) {
-          case "s":
-            console.log("Draw!!");
-            break;
-          case "p":
-            console.log("Scissors cut paper. You win!!");
-            break;
-          case "r":
-            console.log("Rock breaks scissors. You lose!!");
-            break;
-          }
-        }
 }
 
 function main(){
